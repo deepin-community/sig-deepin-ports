5cef4b46551b68b2b92e8e6768eb5f745e2f380c
Yukari Chiba <i@0x7f.cc>
2025-07-25 09:38:19 +0800

    add 250725 weekly report

--- scripts/check-packages.sh
+++ scripts/check-packages.sh
@@ -10,12 +10,14 @@ WORKDIR=$(mktemp -d)
 for COMP in "${COMPS[@]}"; do
   mkdir -p $WORKDIR/$COMP
   for CMPARCH in amd64 riscv64; do
-    curl $REPO_DISTS_URL/$CODENAME/$COMP/binary-$CMPARCH/Packages.gz | gzip -d | grep -e "^Package:" -e "^Version:" | cut -f2 -d ' ' | xargs -n 2 | grep -v "dbgsym" > $WORKDIR/$COMP/$CMPARCH
+    curl $REPO_DISTS_URL/$CODENAME/$COMP/binary-$CMPARCH/Packages.gz | gzip -d | grep-dctrl -s Source,Package,Version -n -v 'dbgsym' | sed 's/^$/@/' | tr '\n' ' ' | tr '@' '\n' | awk 'NF>0 {print $1,$(NF-1),$NF}' | sort | uniq > $WORKDIR/$COMP/$CMPARCH
   done
+  tmpsrc=$(mktemp)
+  curl $REPO_DISTS_URL/$CODENAME/$COMP/source/Sources.gz | gzip -d > $tmpsrc
   comm -23 \
     <(cat $WORKDIR/$COMP/amd64 | cut -f1 -d ' ') \
     <(cat $WORKDIR/$COMP/riscv64 | cut -f1 -d ' ') \
-    > $WORKDIR/$COMP.missing.list
+    | xargs -I @ bash -c "for srcarch in \$(grep-dctrl -w -s Architecture -n -P @ $tmpsrc); do dpkg-architecture -a linux-riscv64 -i \$srcarch && echo @ >> $WORKDIR/$COMP.missing.list && continue; done"
   comm -23 $WORKDIR/$COMP/amd64 $WORKDIR/$COMP/riscv64 > $WORKDIR/$COMP.diff.list
 done
 
